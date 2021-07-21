4e86b99f41f0510442e4a081bf555b4cdc146c16
pangkuanxin <351477478@qq.com>
2021-07-21 18:59:14 +0800

    添加看门口狗功能

--- USER/main.c
+++ USER/main.c
@@ -23,13 +23,56 @@
 #include "stm32f10x.h"
 #include <stdio.h>
 
+void testGPIO(void);
+void initWWDG(void);
+
 int main()
 {
+
+	initWWDG();
+	
 	while (1)
 	{
+		/* Update WWDG counter */
+    WWDG_SetCounter(127);
 	}
-	
-	return 0;
+}
+
+void initWWDG()
+{
+  /* WWDG configuration */
+  /* Enable WWDG clock */
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_WWDG, ENABLE);
+
+/* On Value line devices, WWDG clock counter = (PCLK1 (24MHz)/4096)/8 = 732 Hz (~1366 us)  */
+/* On other devices, WWDG clock counter = (PCLK1(36MHz)/4096)/8 = 1099 Hz (~910 us)  */
+  WWDG_SetPrescaler(WWDG_Prescaler_8);
+
+  /* Set Window value to 80; WWDG counter should be refreshed only when the counter
+    is below 80 (and greater than 64) otherwise a reset will be generated */
+  WWDG_SetWindowValue(80);
+
+  /* - On Value line devices,
+    Enable WWDG and set counter value to 127, WWDG timeout = ~1366 us * 64 = 87.42 ms 
+    In this case the refresh window is: ~1366us * (127-80) = 64.20 ms < refresh window < ~1366us * 64 = 87.42ms
+     - On other devices
+    Enable WWDG and set counter value to 127, WWDG timeout = ~910 us * 64 = 58.25 ms 
+    In this case the refresh window is: ~910 us * (127-80) = 42.77 ms < refresh window < ~910 us * 64 = 58.25ms     
+  */
+  WWDG_Enable(127);
+}
+
+void testGPIO(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB, ENABLE);
+	GPIO_InitTypeDef initStructure;
+	initStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+	initStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	initStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_Init(GPIOA, &initStructure);
+
+	GPIO_SetBits(GPIOA, GPIO_Pin_0);
+	GPIO_SetBits(GPIOA, GPIO_Pin_1);
 }
 
 
